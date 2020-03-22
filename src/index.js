6bbbc431073fb6ade93f757529b322d53e45d4cd
IlonaSoroko <ilona.rumak7@gmail.com>
2020-03-22 19:51:05 +0300

    first try

--- src/index.js
+++ src/index.js
@@ -4,7 +4,44 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let numbers = [];
+    let operators = [];
+
+    expr = expr.split('');
+    for (let i = 0; i < expr.length; i++) {
+        if (expr[i] === " ") {
+            let remove = expr.splice(i, 1);
+        }
+        if (expr[i] === '0') {
+            throw TypeError('TypeError: Division by zero.');
+            break;
+        }
+        if (!isNaN(+expr[i])) {
+            numbers.push(+expr[i]);
+        }
+        else {
+            operators.push(expr[i]);
+        }
+    }
+
+    switch (operators.pop()) {
+        case '+':
+            numbers[numbers.length - 2] = numbers[numbers.length - 2] + numbers.pop();
+            break;
+        case '-':
+            numbers[numbers.length - 2] = numbers[numbers.length - 2] - numbers.pop();
+            break;
+        case '*':
+            numbers[numbers.length - 2] = numbers[numbers.length - 2] * numbers.pop();
+            break;
+        case '/':
+            numbers[numbers.length - 2] = numbers[numbers.length - 2] / numbers.pop();
+            break;
+    }
+
+
+    let result = numbers[0];
+    return result;
 }
 
 module.exports = {
